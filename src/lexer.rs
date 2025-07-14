afcd6a300d159ec7748a147a0667f13861e35417
Peter Aarestad <peter@landscapehub.com>
2025-07-13 20:53:15 -0500

    fixing the driver etc.

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,4 +1,14 @@
-use logos::Logos;
+use logos::{Lexer, Logos};
+
+fn valid_constant(lex: &mut Lexer<Nqc>) -> Option<u64> {
+    let n: u64 = lex.slice().parse().ok()?;
+
+    if !lex.remainder().chars().next()?.is_whitespace() {
+        return None;
+    }
+
+    Some(n)
+}
 
 #[derive(Logos, Debug, PartialEq)]
 #[logos(skip r"[ \t\n\f]+")]
@@ -6,8 +16,8 @@ pub(crate) enum Nqc {
     #[regex("[a-zA-Z_]\\w*")]
     Text,
 
-    #[regex("[0-9]+")]
-    Constant,
+    #[regex("[0-9]+", valid_constant)]
+    Constant(u64),
 
     #[token("int")]
     Int,

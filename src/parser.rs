e67a232dffd7c53321ca6851c4472b2487ed0b48
Peter Aarestad <peter@landscapehub.com>
2025-07-17 16:32:47 -0500

    simplify error enum; recursive parse

--- src/parser.rs
+++ src/parser.rs
@@ -28,45 +28,4 @@ fn parse_recurse(tokens: Vec<TokenResult>) -> Result<Box<NqcNode>, CompilerError
 
 pub(crate) fn parse(tokens: Vec<TokenResult>) -> Result<NqcNode, CompilerError> {
     Ok(NqcNode::Program(parse_recurse(tokens)?))
-    // let Some(fn_result) = tokens.iter().find(|t| {
-    //     if let Ok(NqcToken::Text(_)) = t {
-    //         return true;
-    //     }
-    //
-    //     false
-    // }) else {
-    //     return Err(CompilerError::ParserError(
-    //         "could not find function name".into(),
-    //     ));
-    // };
-    //
-    // let Ok(NqcToken::Text(fn_name)) = fn_result else {
-    //     return Err(CompilerError::ParserError(format!(
-    //         "text parse error: {:#?}",
-    //         &fn_result.clone().err().unwrap()
-    //     )));
-    // };
-    //
-    // let Some(value) = tokens.iter().find(|t| {
-    //     if let Ok(NqcToken::Constant(_)) = t {
-    //         return true;
-    //     }
-    //
-    //     false
-    // }) else {
-    //     return Err(CompilerError::ParserError("could not find constant".into()));
-    // };
-    //
-    // let Ok(NqcToken::Constant(value)) = value else {
-    //     return Err(CompilerError::ParserError(format!(
-    //         "constant parse error: {:#?}",
-    //         fn_result.clone().err().unwrap()
-    //     )));
-    // };
-    //
-    // let constant_node = NqcNode::Constant(*value);
-    // let return_node = NqcNode::Return(Box::new(constant_node));
-    // let fn_node = NqcNode::Function(fn_name.into(), Box::new(return_node));
-    //
-    // Ok(NqcNode::Program(Box::new(fn_node)))
 }
